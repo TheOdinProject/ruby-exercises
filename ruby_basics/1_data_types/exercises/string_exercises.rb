c2451705001eb7d6a98e49d53f9e8f557ea231ef
kokozord <koko_lacteos666@hotmail.com>
2020-09-10 21:05:29 -0500

    create tests and empty exercise methods

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -2,3 +2,39 @@ def concatenation_exercise(string)
   # use concatenation with the empty string below to pass the tests
   ""
 end
+
+def substrings_exercise(string, substring)
+ ##return true if the substring is found in the string
+end
+
+def escape_exercise(string)
+end
+
+def capitalizing_exercise(string)
+	#return the same string, with the first letter capitalized
+end
+
+def uppercase_exercise(string)
+	#return the same string with all of its letters uppercased
+end
+
+def downcase_exercise(string)
+	#return the same string with all of its letters downcased
+end
+
+def empty_exercise(string)
+	#return true if the string is empty, and false otherwise
+end
+
+def length_exercise(string)
+	#return a number equal to the length of the string
+end 
+
+def reverse_exercise(string)
+	#return the same string, with all of its characters reversed
+end
+
+def remover_exercise(string)
+	#return the same string with all of its white space removed
+end
+
