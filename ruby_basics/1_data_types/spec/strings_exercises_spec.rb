7a0aefd47a021a99728d05355315f8c58f29a1df
kokozord <koko_lacteos666@hotmail.com>
2020-09-11 23:02:51 -0500

    add readme

--- ruby_basics/1_data_types/spec/strings_exercises_spec.rb
+++ ruby_basics/1_data_types/spec/strings_exercises_spec.rb
@@ -17,15 +17,15 @@ RSpec.describe 'String Exercises' do
 
 	describe 'substrings exercise' do 
 		
-		it 'returns true if substring is in the string' do 
+		xit 'returns true if substring is in the string' do 
 			expect(substrings_exercise('chocolate', 'late')).to eq(true)
 		end
 
-		it 'returns false if substring is not in string' do 
+		xit 'returns false if substring is not in string' do 
 			expect(substrings_exercise('chocolate', 'milk')).to eq(false)
 		end
 
-		it 'works with multiple words' do 
+		xit 'works with multiple words' do 
 			expect(substrings_exercise('chocolate milk', 'cola')).to eq(true)
 		end
 
@@ -36,15 +36,15 @@ RSpec.describe 'String Exercises' do
 
 	describe 'capitalizing exercise' do 
 		
-		it 'capitalizes a word' do 
+		xit 'capitalizes a word' do 
 			expect(capitalizing_exercise('exercise')).to eq('Exercise')
 		end
 
-		it 'capitalizes only the first word i there are multiple words' do 
+		xit 'capitalizes only the first word i there are multiple words' do 
 			expect(capitalizing_exercise('exercise is good')).to eq('Exercise is good')
 		end
 
-		it 'leaves a capitalized string as is' do 
+		xit 'leaves a capitalized string as is' do 
 			expect(capitalizing_exercise('Capitalized')).to eq('Capitalize')
 		end
 
@@ -52,15 +52,15 @@ RSpec.describe 'String Exercises' do
 
 	describe 'uppercasing exercise' do 
 		
-		it 'uppercases a word' do 
+		xit 'uppercases a word' do 
 			expect(uppercase_exercise('small')).to eq('SMALL')
 		end
 
-		it 'uppercases multiple words' do 
+		xit 'uppercases multiple words' do 
 			expect(uppercase_exercise('make me bigger')).to eq('MAKE ME BIGGER')
 		end
 
-		it 'leaves uppercase letters the same' do 
+		xit 'leaves uppercase letters the same' do 
 			expect(uppercase_exercise('pLeAsE fIx Me')).to eq('PLEASE FIX ME')
 		end
 
@@ -69,15 +69,15 @@ RSpec.describe 'String Exercises' do
 
 	describe 'downcasing exercise' do 
 		
-		it 'downcases a word' do 
+		xit 'downcases a word' do 
 			expect(downcase_exercise('LARGE')).to eq('large')
 		end
 
-		it 'downcases multiple words' do 
+		xit 'downcases multiple words' do 
 			expect(downcase_exercise('MAKE ME SMALLER')).to eq('make me smaller')
 		end
 
-		it 'leaves downcase letters the same' do
+		xit 'leaves downcase letters the same' do
 			expect(downcase_exercise('PlEaSe hElP mE')).to eq('please help me')
 		end
 
@@ -85,11 +85,11 @@ RSpec.describe 'String Exercises' do
 
 	describe 'empty exercise' do 
 
-		it 'returns true if string is empty' do
+		xit 'returns true if string is empty' do
 			expect(empty_exercise('')).to eq(true)
 		end
 
-		it 'returns false if not empty' do 
+		xit 'returns false if not empty' do 
 			expect(empty_exercise('wow')).to eq(false)
 		end
 
@@ -97,15 +97,15 @@ RSpec.describe 'String Exercises' do
 
 	describe 'length exercise' do 
 		
-		it 'returns the length of a word' do 
+		xit 'returns the length of a word' do 
 			expect(length_exercise('longitude')).to eq(9)
 		end
 
-		it 'returns the length of a string with multiple words' do 
+		xit 'returns the length of a string with multiple words' do 
 			expect(length_exercise('i am quite long')).to eq(15)
 		end
 		
-		it 'returns the length of a string of only spaces' do 
+		xit 'returns the length of a string of only spaces' do 
 			expect(length_exercise('     ')).to eq(5)
 		end
 
@@ -113,11 +113,11 @@ RSpec.describe 'String Exercises' do
 
 	describe 'reverse exercise' do
 		
-		it 'reverses a word' do
+		xit 'reverses a word' do
 			expect(reverse_exercise('desrever')).to eq('reversed')
 		end
 
-		it 'reverses multiple words' do 
+		xit 'reverses multiple words' do 
 			expect(reverse_exercise('dnuora kcab')).to eq('back around')
 		end
 
@@ -125,15 +125,15 @@ RSpec.describe 'String Exercises' do
 
 	describe 'remover exercise' do 
 		
-		it 'removes single instances of whitespace' do 
+		xit 'removes single instances of whitespace' do 
 			expect(remover_exercise('white space')).to eq('whitespace')
 		end
 
-		it 'removes multiple instances of whitespace' do 
+		xit 'removes multiple instances of whitespace' do 
 			expect(remover_exercise('many white spaces')).to eq('manywhitespaces')
 		end
 
-		it 'removes multiple spaces in a row' do 
+		xit 'removes multiple spaces in a row' do 
 			expect(remover_exercise('someone get    rid      of         these')).to eq('someonegetridofthese')
 		end
 
