e6d776554b086490c100b26b4f7c2cf44ee23c5e
Kevin Mulhern <kevinmulhern23@gmail.com>
2020-10-06 18:43:04 +0100

    Indent the comments by 2 spaces

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -8,29 +8,29 @@ def substrings_exercise(string, a, b)
 end
 
 def capitalizing_exercise(string)
-	#return the same string, with the first letter capitalized
+  #return the same string, with the first letter capitalized
 end
 
 def uppercase_exercise(string)
-	#return the same string with all of its letters uppercased
+  #return the same string with all of its letters uppercased
 end
 
 def downcase_exercise(string)
-	#return the same string with all of its letters downcased
+  #return the same string with all of its letters downcased
 end
 
 def empty_exercise(string)
-	#return true if the string is empty, and false otherwise
+  #return true if the string is empty, and false otherwise
 end
 
 def length_exercise(string)
-	#return a number equal to the length of the string
+  #return a number equal to the length of the string
 end 
 
 def reverse_exercise(string)
-	#return the same string, with all of its characters reversed
+  #return the same string, with all of its characters reversed
 end
 
 def remover_exercise(string)
-	#return the same string with all of its white space removed
+  #return the same string with all of its white space removed
 end
