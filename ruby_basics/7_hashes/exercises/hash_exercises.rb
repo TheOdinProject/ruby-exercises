f5c3238ce51f260a45eef992b0ba607cb6a7c475
Rachel Moser <moser.rachel@gmail.com>
2020-11-22 15:57:56 -0600

    complete initial methods and tests

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -8,19 +8,41 @@ def favorite_hash(color, number)
   }
 end
 
-def favorite_color(favorite_hash)
+def favorite_color(favorite_list)
   # return the value of the key: color (as a string)
-  favorite_hash['color']
+  favorite_list['color']
 end
 
-def favorite_number(favorite_hash)
+def favorite_number(favorite_list)
   # return the value of the key: number (as a symbol) or 42 if the key is not found
-  favorite_hash.fetch(:number, 42)
+  favorite_list.fetch(:number, 42)
 end
 
+def update_favorite_movie(favorite_list, movie)
+  # Step 1: add/update the key of movie (as a symbol)
+  favorite_list[:movie] = movie
+  # Step 2: return the hash (because Step 1 returns the value of the :movie key )
+  favorite_list
+end
 
+def remove_favorite_number(favorite_list)
+  # Step 1: add/update the key of movie (as a symbol)
+  favorite_list.delete(:number)
+  # Step 2: return the hash (because Step 1 returns the value of the :number key )
+  favorite_list
+end
 
-# Adding and new data
-# Removing data
-# keys and values methods
-# merging
+def favorite_categories(favorite_list)
+  # return the keys of favorite_list
+  favorite_list.keys
+end
+
+def favorite_items(favorite_list)
+  # return the values of favorite_list
+  favorite_list.values
+end
+
+def merge_favorites(original_list, additional_list)
+  # merge the two hashes: original_list and additional_list
+  original_list.merge(additional_list)    
+end
--- ruby_basics/7_hashes/spec/hash_exercises_spec.rb
+++ ruby_basics/7_hashes/spec/hash_exercises_spec.rb
@@ -21,31 +21,114 @@ RSpec.describe 'Hash Exercises' do
   describe 'favorite color exercise' do
 
     it 'returns a string' do
-      my_favorite = { 'color' => 'blue', :number => 65 }
-      expect(favorite_color(my_favorite)).to eq('blue')
+      my_favorites = { 'color' => 'blue', :number => 65 }
+      expect(favorite_color(my_favorites)).to eq('blue')
+    end
+    
+    it 'returns an array' do
+      my_favorites = { 'color' => ['orange', 'green'], :number => { 7 => 'lucky' } }
+      expect(favorite_color(my_favorites)).to eq(['orange', 'green'])
     end
 
-    it 'returns an array' do
-      my_favorite = { 'color' => ['orange', 'green'], :number => { 7 => 'lucky' } }
-      expect(favorite_color(my_favorite)).to eq(['orange', 'green'])
+    it 'returns nil when the key is not found' do
+      my_favorites = { :number => 21, :movie => 'Avengers: Endgame' }
+      expect(favorite_color(my_favorites)).to eq(nil)
     end
   end
 
   describe 'favorite number exercise' do
 
     it 'returns an integer' do
-      my_favorite = { 'color' => 'blue', :number => 65 }
-      expect(favorite_number(my_favorite)).to eq(65)
+      my_favorites = { 'color' => 'blue', :number => 65 }
+      expect(favorite_number(my_favorites)).to eq(65)
     end
 
     it 'returns a hash' do
-      my_favorite = { 'color' => ['orange', 'green'], :number => { 7 => 'lucky' } }
-      expect(favorite_number(my_favorite)).to eq({ 7 => 'lucky' })
+      my_favorites = { 'color' => ['orange', 'green'], :number => { 7 => 'lucky' } }
+      expect(favorite_number(my_favorites)).to eq({ 7 => 'lucky' })
     end
 
     it 'returns the default number when the key is not found' do
-      my_favorite = { 'color' => ['orange', 'green'], :movie => 'Avengers: Endgame' }
-      expect(favorite_number(my_favorite)).to eq(42)
+      my_favorites = { 'color' => ['orange', 'green'], :movie => 'Avengers: Endgame' }
+      expect(favorite_number(my_favorites)).to eq(42)
+    end
+  end
+
+  describe 'update favorite number exercise' do
+
+    it 'returns hash with a new key/value pair when not included' do
+      my_favorites = { 'color' => 'blue', :number => 65 }
+      result = { 'color' => 'blue', :number => 65, :movie => 'Avengers: Endgame' }
+      expect(update_favorite_movie(my_favorites, 'Avengers: Endgame')).to eq(result)
+    end
+
+    it 'returns hash with an updated key/value pair when included' do
+      my_favorites = { 'color' => 'emerald green', :movie => 'Avengers: Endgame' }
+      result = { 'color' => 'emerald green', :movie => 'Avengers: Infinity War' }
+      expect(update_favorite_movie(my_favorites, 'Avengers: Infinity War')).to eq(result)
+    end
+  end
+
+  describe 'remove favorite number exercise' do
+
+    it 'returns hash without key/value pair when included' do
+      my_favorites = { 'color' => 'blue', :number => 65, :movie => 'Avengers: Endgame' }
+      result = { 'color' => 'blue', :movie => 'Avengers: Endgame' }
+      expect(remove_favorite_number(my_favorites)).to eq(result)
+    end
+
+    it 'returns hash when key/value pair is not included' do
+      my_favorites = { 'color' => 'blue', :movie => 'Avengers: Endgame' }
+      expect(remove_favorite_number(my_favorites)).to eq(my_favorites)
+    end
+  end
+
+  describe 'favorite categories exercise' do
+
+    it 'returns an array of the keys' do
+      my_favorites = { 'color' => 'blue', :number => 65, :movie => 'Avengers: Endgame' }
+      expect(favorite_categories(my_favorites)).to eq(['color', :number, :movie])
+    end
+
+    it 'returns an empty array when hash is empty' do
+      my_favorites = {}
+      expect(favorite_categories(my_favorites)).to eq([])
+    end
+
+  end
+
+  describe 'favorite items exercise' do
+
+    it 'returns an array with the string and integer values' do
+      my_favorites = { 'color' => 'blue', :number => 65, :movie => 'Avengers: Endgame' }
+      expect(favorite_items(my_favorites)).to eq(['blue', 65, 'Avengers: Endgame'])
+    end
+
+    it 'returns an array with the array and hash values' do
+      my_favorites = { 'color' => ['orange', 'green'], :number => { 7 => 'lucky' } }
+      expect(favorite_items(my_favorites)).to eq([['orange', 'green'], { 7 => 'lucky' }])
+    end
+
+    it 'returns an empty array when hash is empty' do
+      my_favorites = {}
+      expect(favorite_items(my_favorites)).to eq([])
+    end
+  end
+
+  describe 'favorite items exercise' do
+
+    it 'returns a merged hash' do
+      my_favorites = { 'color' => 'blue', :number => 65 }
+      new_favorites = { :movie => 'Avengers: Endgame' }
+      result = { 'color' => 'blue', :number => 65, :movie => 'Avengers: Endgame' }
+      expect(merge_favorites(my_favorites, new_favorites)).to eq(result)
+    end
+
+    it 'returns a updated hash' do
+      my_favorites = { 'color' => 'blue', :number => 65, :movie => 'Avengers: Endgame' }
+      new_favorites = { :movie => 'Avengers: Infinity War' }
+      result = { 'color' => 'blue', :number => 65, :movie => 'Avengers: Infinity War' }
+      expect(merge_favorites(my_favorites, new_favorites)).to eq(result)
     end
   end
 
