9e83b50db52081dbafe83d2e59028adefdac54ef
Rachel Moser <moser.rachel@gmail.com>
2020-12-04 01:01:12 -0600

    update file format and add predicate method

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -1,8 +1,28 @@
-# write a method named #final_list that returns an array with 2 chained methods:
-# sort the items alphabetically (sort will work numerically too)
-# remove any duplicates by making sure every item is unique
-def final_list(list)
-  list.sort.uniq
+# Now it your turn to write the entire method to pass tests one at a time
+
+# method name: #alphabetical_list
+# parameter: games (an array)
+# return value: games with the two following array methods chained together
+# sort (orders the items alphabetically)
+# uniq (removes any duplicates)
+def alphabetical_list(games)
+  games.sort.uniq
 end
 
+# method name: #divisible_by_four?
+# parameter: year
+# return value: true if the year is evenly divided by 4, otherwise return false
+def divisible_by_four?(year)
+  year % 4 == 0
+end
 
+# method name:
+# parameter: 
+# return value: 
+def capitalize_string(string)
+  string.upcase!
+end
+
+# method name:
+# parameter: 
+# return value: 
--- ruby_basics/8_methods/spec/method_exercises_spec.rb
+++ ruby_basics/8_methods/spec/method_exercises_spec.rb
@@ -4,15 +4,33 @@ require_relative '../exercises/method_exercises'
 RSpec.describe 'Method Exercises' do
   describe 'chaining methods exercise' do
 
-    it 'returns an sorted array of names' do
-      names = ['Carl', 'Bob', 'Amy', 'Bob']
-      expect(final_list(names)).to eq(['Amy', 'Bob', 'Carl'])
+    it 'returns an sorted array removing one duplicate' do
+      games = ['Chess', 'Scrabble', 'Othello', 'Chess']
+      expect(alphabetical_list(games)).to eq(['Chess', 'Othello', 'Scrabble'])
     end
     
     # remove the 'x' from the line below to unskip the test
-    it 'returns an sorted array of numbers' do
-      numbers = [3, 5, 2, 2]
-      expect(final_list(numbers)).to eq([2, 3, 5])
+    it 'returns an sorted array removing multiple duplicates' do
+      games = ['Tennis', 'Baseball', 'Baseball', 'Soccer', 'Tennis', 'Tennis']
+      expect(alphabetical_list(games)).to eq(['Baseball', 'Soccer', 'Tennis'])
+    end
+  end
+
+  describe 'divisible by four exercise' do
+
+    it 'returns true when the year is divisible by 4' do
+      expect(divisible_by_four?(2024)).to be true
+    end
+
+    it 'returns false when the year is not divisible by 4' do
+      expect(divisible_by_four?(1999)).to be false
+    end
+  end
+
+  describe 'bang method exercise' do
+
+    it 'returns ...' do
+      expect(capitalize_string('scream')).to eq('SCREAM')
     end
   end
 end
@@ -20,12 +38,11 @@ end
 # Lesson: https://www.theodinproject.com/courses/ruby-programming/lessons/methods
 
 # Creating their own methods - 2 or 3 exercises for this
-# different types of arguments
-# different return values
-# make your own predicate method
-  # modulo - write a method called divisible_by_four? that returns
-# make your own bang method
-# I think for the last two points we could get them to reimplement something like the array empty? method and the string upcase! method.
+# different types of arguments -> clarify meaning?
+# MEANING -> default parameters?
+# different return values 
+# DONE - make your own predicate method (suggestion: array empty?)
+# make your own bang method (suggestion: string upcase!)
   
 # Ideas:
 # - loops
@@ -35,4 +52,4 @@ end
 # - random # - write a method called dice_roll that returns a random number between 1 - 12
 # - abs number
 
-#   return number if number.between?(0, 9)
+#  return number if number.between?(0, 9) -> example of years (in a decade or century)
