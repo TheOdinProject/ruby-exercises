f5671520f80c37564f17749121f4b37106af417d
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-16 18:34:26 -0300

    Add all methods, finishing 8_methos section

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,23 +7,35 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(number)
+  number.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection 
 
+def common_sports(current_sports, favorite_sports)
+  current_sports.intersection(favorite_sports)
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list(games)
+  games.sort.uniq
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
 
+def lucky_number(number = 7)
+  "Today's lucky number is #{number}"
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
@@ -31,6 +43,10 @@
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+def ascii_code(character)
+  return 'Input Error' unless character.length == 1
+  character.ord
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -40,9 +56,22 @@
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
 
+def pet_pun(animal)
+  if animal == "cat"
+    puts "Cats are purr-fect!"
+  elsif animal == "dog"
+    puts "Dogs are paw-some!"
+  else
+    puts "I think #{animal}s have pet-tential!"
+  end
+end
+
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
 
+def twenty_first_century?(year)
+  year.between?(2001, 2100)
+end
