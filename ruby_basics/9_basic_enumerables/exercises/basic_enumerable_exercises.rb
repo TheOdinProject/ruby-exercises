6ce9da9af29200ccf0e4f678a756dc95f8bd15e4
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-16 19:29:12 -0300

    Solve all but the least exercise. The last done needed a ternary to 'reset' the accumulator because it tends to be nil if not properly set - https://stackoverflow.com/questions/12488579/reduce-returns-nil-on-summation/12488614

--- ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/9_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -1,22 +1,26 @@
 def display_current_inventory(inventory_list)
   # use #each to iterate through each item of the inventory_list (a hash)
   # use puts to output each list item "<key>, quantity: <value>" to console
+  inventory_list.each  { |key, value| puts "#{key}, quantity: #{value}" }
 end
 
 def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
+  guesses.each_with_index { |guess, idx| puts "Guess ##{idx + 1} is #{guess}" }
 end
 
 def find_absolute_values(numbers)
   # use #map to iterate through each item of the numbers (an array)
   # return an array of absolute values of each number
+  numbers.map { |number| number.abs }
 end
 
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
+  inventory_list.select { |_, value| value < 4 }
 end
 
 def find_longest_word(word_list)
@@ -24,12 +28,14 @@ def find_longest_word(word_list)
   # return the longest word in the word_list
   # hint: the result of each iteration should be the accumulator when its 
   # length is greater than word.length (otherwise the result should be the word)
+  word_list.reduce { |acc, word| acc.length > word.length ? acc : word }
 end
 
 def find_longer_words(word_list, base_word)
   # use #reduce to iterate through each item of the word_list (an array)
   # return an array of words that is longer than the base_word (a string)
   # hint: use a new array or an empty array as the initial accumulator value
+  word_list.reduce([]) { |acc, word| word.length > base_word.length ? acc << word : acc }  
 end
 
 def find_word_lengths(word_list)
