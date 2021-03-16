97618ab863bba95c243a890b0f46ccf4596eb591
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-16 16:12:42 -0300

    Add methods until (exclusive) the array_concatenation

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -5,34 +5,39 @@ end
 
 def nested_array(number)
   # return an array containing nested empty arrays the given number of times
+  Array.new(Array.new(number, []))
 end
 
 def first_element(array)
   # return the first element of the array
+  array.first
 end
 
 def third_element(array)
   # return the third element of the array
+  array[2]
 end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
+  array.pop(3)
 end
 
 def add_element(array)
   # add an element (of any value) to the array
+  array << 1
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-
+  array.pop
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array
 end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-
+  array.shift(3)
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
 end
