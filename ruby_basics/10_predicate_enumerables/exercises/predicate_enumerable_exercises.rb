09e7c89c868b12c25d5a76eaf43ee24299cfb36c
Rachel Moser <moser.rachel@gmail.com>
2021-03-27 16:26:12 -0500

    Add predicate enumerable exercises and tests

---
+++ ruby_basics/10_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -0,0 +1,19 @@
+def coffee_drink?(drink_list)
+  # use #include? to return true when the drink_list (array) contains the string "coffee" or "espresso"
+end
+
+def correct_guess?(guess_list, answer)
+  # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+end
+
+def recent_years?(year_list)
+  # use #all? to return true when all of the years in the year_list (array) are between 2011 and 2021
+end
+
+def correct_format?(word_list)
+  # use #none? to return true when none of the words in the word_list (array) are in upcase
+end
+
+def valid_scores?(score_list, perfect_score)
+  # use #one? to return true when only one value in the score_list (hash) is equal to the perfect_score (number)
+end
