494a565c6020dd7bbc41d889c51d1e2ffbdc2ea1
Sully <a.m.sullivan.github@gmail.com>
2021-06-15 23:56:48 -0400

    Add instructions to nested_hash_exercises

--- ruby_basics/11_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/11_nested_collections/exercises/nested_hash_exercises.rb
@@ -1,33 +1,32 @@
-def add_information_about_language(hash, language_name, info_key, info_value)
-  hash[language_name][info_key] = info_value
-  hash
+def add_information_about_language(languages, language_name, info_key, info_value)
+  # Take languages (a hash with language names as keys and information about
+  # each language in a nested hash as the corresponding value) and add the
+  # key/value pair info_key/info_value to the nested hash with key language_name
+  # then return the updated languages hash
 end
 
-def add_language(hash, language_name, language_info_value)
-  hash[language_name] = language_info_value
-  hash
+def add_language(languages, language_name, language_info_value)
+  # Take languages and add the key/value pair language_name/language_info_value
+  # to it, then return languages
 end
 
-def delete_information_about_language(hash, language_name, info_key)
-  hash[language_name].delete(info_key)
-  hash
+def delete_information_about_language(languages, language_name, info_key)
+  # Take languages and delete the key/value pair with key info_key from
+  # language_name, then return languages
 end
 
-def delete_language(hash, language_name)
-  hash.delete(language_name)
-  hash
+def delete_language(languages, language_name)
+  # Take languages and delete the language_name key/value pair, then return
+  # languages
 end
 
-def delete_beautiful_languages(hash)
-  hash.reject do |_language, data|
-    data.fetch(:is_beautiful?)
-  end
+def delete_beautiful_languages(languages)
+  # Take languages and delete any languages that have the key/value pair
+  # { is_beautiful => true } listed in their information
 end
 
-def find_language_facts(hash, fact_index)
-  result = []
-  hash.each_key do |key|
-    result.push(hash[key].fetch(:facts)[fact_index])
-  end
-  result
+def find_language_facts(languages, fact_index)
+  # Take languages (now with additional facts added to each language with the
+  # key facts and value of an array of strings) and return an array containing
+  # the fact that each language has at fact_index of its facts array
 end
