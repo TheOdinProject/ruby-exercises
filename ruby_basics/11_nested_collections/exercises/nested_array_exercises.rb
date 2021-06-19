5db6f5d4b69a57d2ae14a63701241fdb4257fe9e
Sully <a.m.sullivan.github@gmail.com>
2021-06-18 22:03:41 -0400

    Add array_of_hash_objects example in nested_array_exercises

--- ruby_basics/11_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/11_nested_collections/exercises/nested_array_exercises.rb
@@ -44,6 +44,13 @@ def find_favorite(array_of_hash_objects)
   # pair :is_my_favorite? => true. If no hash returns the value true to the key
   # :is_my_favorite? it should return nil
 
+  # array_of_hash_objects will look something like this:
+  # [
+  #   { name: 'Ruby', is_my_favorite?: true },
+  #   { name: 'JavaScript', is_my_favorite?: false },
+  #   { name: 'HTML', is_my_favorite?: false }
+  # ]
+
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
 end
