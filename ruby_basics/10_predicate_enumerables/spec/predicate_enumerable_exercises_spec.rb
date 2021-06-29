3773c4ed47b7120250449712bf9a92f9eba8aee9
Alex Wilson <47777467+Adub648@users.noreply.github.com>
2021-06-29 16:43:17 +1000

    Updated variables to match predicate_enermerable_exercises.rb

--- ruby_basics/10_predicate_enumerables/spec/predicate_enumerable_exercises_spec.rb
+++ ruby_basics/10_predicate_enumerables/spec/predicate_enumerable_exercises_spec.rb
@@ -6,102 +6,102 @@ RSpec.describe 'Predicate Enumerable Exercises' do
   describe 'coffee drink exercise' do
 
     it 'returns true when coffee is included' do
-      list = ["coffee", "water", "tea"]
-      expect(coffee_drink?(list)).to be true
+      drink_list = ["coffee", "water", "tea"]
+      expect(coffee_drink?(drink_list)).to be true
     end
     
     # remove the 'x' from the line below to unskip the test
     xit 'returns true when espresso is included' do
-      list = ["milk", "juice", "espresso"]
-      expect(coffee_drink?(list)).to be true
+      drink_list = ["milk", "juice", "espresso"]
+      expect(coffee_drink?(drink_list)).to be true
     end
 
     xit 'returns false when coffee or espresso is not included' do
-      list = ["tea", "water", "milk"]
-      expect(coffee_drink?(list)).to be false
+      drink_list = ["tea", "water", "milk"]
+      expect(coffee_drink?(drink_list)).to be false
     end
 
     xit 'returns false when the list is empty' do
-      list = []
-      expect(coffee_drink?(list)).to be false
+      drink_list = []
+      expect(coffee_drink?(drink_list)).to be false
     end
   end
 
   describe 'correct guess exercise' do
 
     xit 'returns true when the list contains the answer' do
-      list = [2, 3, 4, 5]
+      guess_list = [2, 3, 4, 5]
       answer = 5
-      expect(correct_guess?(list, answer)).to be true
+      expect(correct_guess?(guess_list, answer)).to be true
     end
 
     xit 'returns false when the list does not contain the answer' do
-      list = [6, 7, 8, 9]
+      guess_list = [6, 7, 8, 9]
       answer = 5
-      expect(correct_guess?(list, answer)).to be false
+      expect(correct_guess?(guess_list, answer)).to be false
     end
 
     xit 'returns false when the list is empty' do
-      list = []
+      guess_list = []
       answer = 5
-      expect(correct_guess?(list, answer)).to be false
+      expect(correct_guess?(guess_list, answer)).to be false
     end
   end
 
   describe 'recent years exercise' do
 
     xit 'returns true when all of the years are between 2011 and 2021' do
-      list = [2011, 2021, 2016]
-      expect(recent_years?(list)).to be true
+      year_list = [2011, 2021, 2016]
+      expect(recent_years?(year_list)).to be true
     end
 
     xit 'returns false when one year is not between 2011 and 2021' do
-      list = [2018, 2001, 2014]
-      expect(recent_years?(list)).to be false
+      year_list = [2018, 2001, 2014]
+      expect(recent_years?(year_list)).to be false
     end
 
     xit 'returns true when the list is empty' do
-      list = []
-      expect(recent_years?(list)).to be true
+      word_list = []
+      expect(recent_years?(word_list)).to be true
     end
   end
 
   describe 'correct format exercise' do
 
     xit 'returns true when none of the words in the list are in upcase' do
-      list = ["Pepsi", "Coke", "Dr. Pepper"]
-      expect(correct_format?(list)).to be true
+      word_list = ["Pepsi", "Coke", "Dr. Pepper"]
+      expect(correct_format?(word_list)).to be true
     end
 
     xit 'returns false when at least one word in the list is in upcase' do
-      list = ["PEPSI", "Coke", "Dr. Pepper"]
-      expect(correct_format?(list)).to be false
+      word_list = ["PEPSI", "Coke", "Dr. Pepper"]
+      expect(correct_format?(word_list)).to be false
     end
 
     xit 'returns true when the list is empty' do
-      list = []
-      expect(correct_format?(list)).to be true
+      word_list = []
+      expect(correct_format?(word_list)).to be true
     end
   end
 
   describe 'valid scores exercise' do
 
     xit 'returns true when only one score is a 10' do
-      list = { easy_to_read: 10, uses_best_practices: 8, clever: 7 }
+      score_list = { easy_to_read: 10, uses_best_practices: 8, clever: 7 }
       perfect_score = 10
-      expect(valid_scores?(list, perfect_score)).to be true
+      expect(valid_scores?(score_list, perfect_score)).to be true
     end
 
     xit 'returns false when more than one score is a 10' do
-      list = { easy_to_read: 10, uses_best_practices: 10, clever: 9 }
+      score_list = { easy_to_read: 10, uses_best_practices: 10, clever: 9 }
       perfect_score = 10
-      expect(valid_scores?(list, perfect_score)).to be false
+      expect(valid_scores?(score_list, perfect_score)).to be false
     end
 
     xit 'returns false when the list is empty' do
-      list = {}
+      score_list = {}
       perfect_score = 10
-      expect(valid_scores?(list, perfect_score)).to be false
+      expect(valid_scores?(score_list, perfect_score)).to be false
     end
   end
 end
