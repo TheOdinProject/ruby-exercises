0017eecd380e156c51d73e4efde0b7fbfbdfebf7
Sebastian <sebastianwebp@gmail.com>
2021-08-06 09:42:44 -0400

    data-types homework finished

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -5,32 +5,43 @@ end
 
 def subtract(a, b)
   # return the result of subtracting b from a
+  a - b
 end
 
 def multiply(a, b)
   # return the result of multiplying a times b
+  a * b
 end
 
 def divide(a, b)
   # return the result of dividing a by b
+  a / b
 end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+   a % b
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  a.to_f / b.to_f
 end
 
 def string_to_number(string)
   # return the result of converting a string into an integer
+  number = string.to_i
+  return number
 end
 
 def even?(number)
   # return true if the number is even (hint: use integer's even? method)
+  even_number = number.even?
+  return even_number
 end
 
 def odd?(number)
   # return true if the number is odd (hint: use integer's odd? method)
+  odd_number = number.odd?
+  return odd_number
 end
