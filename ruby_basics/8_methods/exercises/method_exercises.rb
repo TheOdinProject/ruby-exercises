05f58fd860c522ea750ebfe7a84c305ab09fd7de
Sebastian <sebastianwebp@gmail.com>
2021-08-06 20:22:18 -0400

    methods assignment finished

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,22 +7,37 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(number)
+ number.chr
+end
+
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
 
+def common_sports(current_sports , favorite_sports)
+    current_sports & favorite_sports
+end
+
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
+def alphabetical_list(games)
+    games.sort.uniq
+end
 
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+def lucky_number(number = 7)
+    p "Today's lucky number is #{number}"
+end
+
 
 
 # method name: #ascii_code
@@ -30,6 +45,14 @@
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
+def ascii_code(character)
+    if character.length != 1
+        return "Input Error"
+    else
+        character.ord
+    end
+    
+end
 
 
 # method name: #pet_pun
@@ -39,10 +62,26 @@
 # console output: if animal is 'dog', 'Dogs are paw-some!' (awesome)
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
+def pet_pun(animal)
+    if animal == 'cat'
+        puts 'Cats are purr-fect!'
+    elsif animal == 'dog'
+        puts 'Dogs are paw-some!'
+    else
+        puts "I think #{animal}s have pet-tential!"
+    end
+end
 
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
+def twenty_first_century?(year)
+    if year >= 2001 && year <= 2100
+        return true
+    else
+        return false
+    end
+end
 
