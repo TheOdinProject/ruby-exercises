eb5ff241d27ce6d6481c008cd8432603b48e4a12
mikhail <mikhailgrigoriev92@gmail.com>
2022-02-28 12:33:11 -0500

    Create rough draft of broken exercises to be fixed by debugging

---
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -0,0 +1,39 @@
+# use the stack trace to debug the following method
+# method name: #array_in_hash
+# parameter: ?
+# return value: smallest value in array in a hash
+
+# use the stack trace to debug the following method
+# method name: #hash_in_array
+# parameter: ?
+# return value: greatest value in hash in array
+
+def array_length(array)
+  # use puts to debug this method, so that it
+  # returns the length of the array
+end
+
+def include?(array, value)
+  # use puts to debug this method, so that it
+  # returns true if the array includes the value
+end
+
+def join(array, separator)
+  # use puts to debug this method, so that it
+  # returns the result of joining the array with the separator
+end
+
+def favorite_categories(favorite_list)
+  # use pry to debug this method, so that it
+  # returns the keys of favorite_list
+end
+
+def favorite_items(favorite_list)
+  # use pry to debug this method, so that it
+  # returns the values of favorite_list
+end
+
+def favorite_number(favorite_list)
+  # use pry to debug this method, so that it
+  # returns the value of the number key or 42 if the key is not found
+end
