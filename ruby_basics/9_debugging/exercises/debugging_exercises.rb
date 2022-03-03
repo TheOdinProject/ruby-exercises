91053e3dd273c5fe22a686dd19a1c3c665032ca5
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-03 15:26:06 -0500

    Finish second stack trace exercise
    Along with accompanying tests. The .rspec file was also edited to provide documentation upon running tests

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -9,7 +9,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_value
+        smallest_value = current_valu
       end
     end
   end
@@ -17,8 +17,18 @@ def decrement_smallest_value(nested_array)
 end
 
 # use the stack trace to debug the following method
+# Don't look at this method prior to running the test
+# Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-
+  greatest_value = nested_array.flatten.min
+  nested_array.each do |array|
+    array.each do |current_value|
+      if greatest_value < nil
+        greatest_value = current_value
+      end
+    end
+  end
+  greatest_value += 1
 end
 
 def array_length(array)
--- ruby_basics/9_debugging/spec/debugging_exercises_spec.rb
+++ ruby_basics/9_debugging/spec/debugging_exercises_spec.rb
@@ -4,13 +4,24 @@ require_relative '../exercises/debugging_exercises'
 RSpec.describe 'Debugging Exercises' do
   describe 'decrement_smallest_value' do
 
-    # remove the 'x' from the line below to unskip the test
-    it 'returns 0 after finding the smallest value of 1, and decrementing by 1' do
+    it 'Returns 0 after you use the stack trace to find and fix the error' do
       nested_array = [[3, 4], [1, 2]]
 
       smallest_value_decremented = decrement_smallest_value(nested_array)
 
       expect(smallest_value_decremented).to eq(0)
+    end
+  end
+
+  describe 'increment_greatest_value' do
+
+    # remove the 'x' from the line below to unskip the test
+    xit 'Returns 5 after you use the stack trace to find and fix the error' do
+      nested_array = [[3, 4], [1, 2]]
+
+      greatest_value_incremented = increment_greatest_value(nested_array)
+
+      expect(greatest_value_incremented).to eq(5)
     end
   end
 end
