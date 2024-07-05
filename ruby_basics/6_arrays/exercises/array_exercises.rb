b8443f4bf8b35ab589fc4ece8584f39bcccb0331
samuel <samiehave9@gmail.com>
2024-07-05 04:14:23 +0300

    Array Exercise

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -15,52 +15,69 @@ end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
+  array.last[3]
 end
 
 def add_element(array)
   # add an element (of any value) to the array
+  array = [1,2,3,4,5,6,7]
+  array.unshift(8)
+  array << 9
+  array.push(5)
+  array.insert(2, 'boy')
+  arr.insert(3, 'orange', 'pear', 'grapefruit')
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-
+  array.pop
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array
 end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-
+  array.shift(3)
+  array.delete_at(3)
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
 end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
+  original + additional
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
+  original - comparison
 end
 
 def empty_array?(array)
   # return true if the array is empty
+  array.empty?
 end
 
 def reverse(array)
   # return the reverse of the array
+  array.reverse
 end
 
 def array_length(array)
   # return the length of the array
+  arrary.length
+  array.count
+  array.size
 end
 
 def include?(array, value)
   # return true if the array includes the value
+  array.include?(value)
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
+  array.join(separator)
 end
 
 # ITERATING OVER ARRAYS
