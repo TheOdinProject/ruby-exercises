135fb7222d97ff640d9378b86d4285f45f79a774
samuel <samiehave9@gmail.com>
2024-07-07 12:35:14 +0300

    Fetch

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -45,14 +45,17 @@ def create_favorite_hash(color, number)
   # return a hash with the following key/value pairs:
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
+  favorite_list = {color: color, number: number}
 end
 
 def favorite_color(favorite_list)
   # return the value of the color key
+  favorite_color = favorite_list[:color]
 end
 
 def favorite_number(favorite_list)
   # use #fetch to return the value of the number key or 42 if the key is not found
+  favorite_list.fetch(:number, 42)
 end
 
 def update_favorite_movie(favorite_list, movie)
