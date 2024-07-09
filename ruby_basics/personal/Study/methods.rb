b9adfdf88d6ceb96f5107b4c7d3d1e43b4c59eab
samuel <samiehave9@gmail.com>
2024-07-09 05:38:05 +0300

    Exercise

--- ruby_basics/personal/Study/methods.rb
+++ ruby_basics/personal/Study/methods.rb
@@ -317,11 +317,34 @@ def decorate_greeting(person)
 end
 
 decorate_greeting("John")
-decorate_greeting(1)
+# decorate_greeting(1) # Causes an ERROR
 
+# METHOD DEFINITION ORDER
+def top
+    bottom
+end
 
+def bottom
+    puts "Reached the bottom"
+end
 
+top
 
+# EXERCISE
+# Write a program that checks if the sequence of characters "lab" exists in the following strings. If it does exist, print out the word.
+def has_lab?(string)
+    if string =~ /lab/i
+        puts "We got a Match!"
+    else 
+        puts "No Match found!"
+    end
+end
+
+has_lab?('laboratory')
+has_lab?("experiment")
+has_lab?("Pans Labyrinth")
+has_lab?("elaborate")
+has_lab?("polar bear")
 
 
 
