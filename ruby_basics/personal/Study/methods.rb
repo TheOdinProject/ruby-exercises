1ad2bbcc24ca92a84be594f95daa1d84e51d093b
samuel <samiehave9@gmail.com>
2024-07-09 15:57:43 +0300

    Delete_if

--- ruby_basics/personal/Study/methods.rb
+++ ruby_basics/personal/Study/methods.rb
@@ -399,23 +399,81 @@ end
 
 puts new_arr
 
+# Question 8: Append 11 to the end of the original array. Prepend 0 to the beginning.
+arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr.push(11)
+# arr << 11
+arr.unshift(0)
+puts arr
 
+# Question 9: Get rid of 11. And append a 3.
+arr.pop
+arr.push (3)
+puts arr
 
+# Question 10: Get rid of duplicates without specifically removing any one value.
+arr.uniq!
+puts arr
 
+# Question 11: What's the major difference between an Array and a Hash?
 
+# An array is an ordered list of elements, while a hash is a collection of key-value pairs.
 
+# Question 12: Create a hash using both Ruby syntax styles.
+person = {name: 'Bob', occupation: 'Web Developer', hobbies: 'Joggong'}
+person1 = {:name => 'Bob', :occupation => 'Web Developer', :hobbies => 'Joggong'} 
 
+# Question 13: Suppose you have a hash h = {a:1, b:2, c:3, d:4}
+# 1. Get the value of key `:b`.
+# 2. Add to this hash the key:value pair `{e:5}`
+# 3. Remove all key:value pairs whose value is less than 3.5
+h = {a:1, b:2, c:3, d:4}
+h[:b]
+h[:e] = 5
+h[:f] = 6
+h.delete_if { |k, v| v < 3.5}
+puts h
+h.delete_if do |k, v|
+    v < 3.5
+end
 
+puts h
 
+# Question 14: Given the following data structures, write a program that copies the information from the array into the empty hash that applies to the correct person.
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
+# Expected output:
+#  {
+#    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
 
+# While Working with Hashes, Always remember that you can access the value of a key in a hash by using the hash[key] syntax. You can also set the value of a key in a hash by using the hash[key] = value syntax.
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
 
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
 
+puts contacts
+# Output: # Expected output:
+#  {
+#    "Joe Smith"=>{:email=>"joe@email.com", :address=>"123 Main st.", :phone=>"555-123-4567"},
+#    "Sally Johnson"=>{:email=>"sally@email.com", :address=>"404 Not Found Dr.",  :phone=>"123-234-3454"}
+#  }
 
+# Question 15: Use Ruby's Array method delete_if and String method start_with? to delete all of the words that begin with an "s" in the following array.
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+arr.delete_if do |word| 
+    word.start_with?("s" || "w")
+end
 
-
-
+puts arr
 
 
 
