bec78716aed35c5c2283e53961337c748846a730
samuel <samiehave9@gmail.com>
2024-07-10 04:44:37 +0300

    SHIFT Method

--- ruby_basics/personal/Study/methods.rb
+++ ruby_basics/personal/Study/methods.rb
@@ -502,8 +502,27 @@ end
 
 # Note that this exercise is only concerned with dealing with 1 entry in the contacts hash, like this:
 
-contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
-contacts = {"Joe Smith" => {}}
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+                ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
+
+# contacts.each do |name, hash|
+#     fields.each do |field|
+#         hash[field] = contact_data.shift
+#     end
+# end
+
+contacts.each_with_index do |(name, hash), idx|
+    fields.each do |field|
+        hash[field] = contact_data[idx].shift
+    end
+end
+
+p contacts
+
+
+
 
 
 
