3b9878afdd80c769c8639af6b47c47d2efae8d5b
Josh Smith <jmsmith1018@gmail.com>
2025-07-03 19:29:49 -0600

    Add total integers exercise

---
+++ recursion/3_total_integers/exercises/total_integers_exercises.rb
@@ -0,0 +1,6 @@
+def total_integers(array)
+  # Count the total number of integers inside of the given array
+  # The array may be nested, and the integers inside these "inner" layers must also be counted
+  #
+  # Example: `total_integers([0, 1, [5]]) == 3`
+end
